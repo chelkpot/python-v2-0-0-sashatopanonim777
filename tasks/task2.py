c9584a54bee9b2a8eff2ed5561b465be48a8f565
саша чеснок <some0elsee@gmail.com>
2025-09-29 11:20:39 +0500

    пацетко

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,14 +2,13 @@
 
 def solve():
 # Ниже пишите решение задачи
-    x, y, z = map(int, input("").split())
-    
+    x, y, z = map(int, input().split())
+
     pencil_price = 3
     pen_price = pencil_price + 2
     marker_price = pen_price + 7
-    
+
     total_cost = x * pencil_price + y * pen_price + z * marker_price
-    
     print(total_cost)
 
    
